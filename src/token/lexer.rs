6d876fa25c7289ea4446ed374eb6976358ef28dc
Jake Everhart <jake.a.everhart@gmail.com>
2022-11-12 23:48:54 -0600

    Parsing keywords

--- src/token/lexer.rs
+++ src/token/lexer.rs
@@ -19,7 +19,7 @@ impl<'a> Lexer<'a> {
 
     fn take_until(&mut self, condition: impl Fn(&char) -> bool) -> Vec<char> {
         let mut taken = Vec::new();
-        while let Some(next) = self.input.next_if(&condition) {
+        while let Some(next) = self.input.next_if(|c| !condition(c)) {
             taken.push(next);
         }
         return taken;
@@ -35,7 +35,21 @@ impl<'a> Lexer<'a> {
             '}' => Token::RBrace,
             ',' => Token::Comma,
             ';' => Token::Semicolon,
-            _ => Token::Illegal,
+            c => {
+                if c.is_alphabetic() {
+                    // Capture remainder of alphanumeric word
+                    let mut word = vec![c];
+                    let rest = self.take_until(|x| !is_keyword_char(x));
+                    word.extend(rest.iter());
+                    let word_string: String = word.into_iter().collect();
+                    return map_word_to_token(&word_string);
+                }
+                if c.is_digit(10) {
+                    // Capture remainder of number
+                    todo!()
+                }
+                Token::Illegal
+            }
         }
     }
 }
@@ -44,6 +58,7 @@ impl<'a> Iterator for Lexer<'a> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
+        self.take_until(|x| !x.is_whitespace());
         match self.input.next() {
             Some(c) => Some(self.pull_next_token(c)),
             None => None,
@@ -51,9 +66,26 @@ impl<'a> Iterator for Lexer<'a> {
     }
 }
 
+static KEYWORDS: phf::Map<&'static str, Token> = phf::phf_map! {
+    "fn" => Token::Function,
+    "let" => Token::Let
+};
+
+fn is_keyword_char(c: &char) -> bool {
+    c.is_alphanumeric() || c.eq(&'_')
+}
+
+fn map_word_to_token(word: &str) -> Token {
+    if KEYWORDS.contains_key(word) {
+        return KEYWORDS.get(word).unwrap().clone();
+    }
+    Token::Identifier(String::from(word))
+}
+
 #[cfg(test)]
 mod tests {
     use crate::token::types::Token;
+    use pretty_assertions::assert_eq;
 
     use super::Lexer;
 
@@ -70,13 +102,61 @@ mod tests {
             Token::Comma,
             Token::Semicolon,
         ];
-        let mut lexer = Lexer::new(input);
-        let mut counter = 0;
-        for expected_token in expected.iter() {
-            let actual_token = lexer.next().unwrap();
-            assert_eq!(expected_token, &actual_token);
-            counter += 1;
-        }
-        assert_eq!(expected.len(), counter);
+        assert_tokens(expected, Lexer::new(input));
+    }
+
+    #[test]
+    fn symbols_and_keywords() {
+        let input = "
+            let five = 5;
+            let ten = 10;
+            let add = fn(x, y) {
+            x + y;
+            };
+            let result = add(five, ten);
+        ";
+        let expected = vec![
+            Token::Let,
+            Token::Identifier(String::from("five")),
+            Token::Assignment,
+            Token::Integer(5),
+            Token::Semicolon,
+            Token::Let,
+            Token::Identifier(String::from("ten")),
+            Token::Assignment,
+            Token::Integer(10),
+            Token::Semicolon,
+            Token::Let,
+            Token::Identifier(String::from("add")),
+            Token::Assignment,
+            Token::Function,
+            Token::LParen,
+            Token::Identifier(String::from("x")),
+            Token::Comma,
+            Token::Identifier(String::from("y")),
+            Token::RParen,
+            Token::LBrace,
+            Token::Identifier(String::from("x")),
+            Token::Plus,
+            Token::Identifier(String::from("y")),
+            Token::Semicolon,
+            Token::RBrace,
+            Token::Semicolon,
+            Token::Let,
+            Token::Identifier(String::from("result")),
+            Token::Assignment,
+            Token::Identifier(String::from("add")),
+            Token::LParen,
+            Token::Identifier(String::from("five")),
+            Token::Comma,
+            Token::Identifier(String::from("ten")),
+            Token::RParen,
+            Token::Semicolon,
+        ];
+        assert_tokens(expected, Lexer::new(input));
+    }
+
+    fn assert_tokens(expected: Vec<Token>, actual: impl Iterator<Item = Token>) {
+        assert_eq!(expected, actual.into_iter().collect::<Vec<Token>>());
     }
 }
